54b2fb76c718b18fabdc59fe6a377dac907cb852
vim12345 <vk2699945@gmail.com>
2024-01-06 15:55:22 +0530

    new

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -293,7 +293,7 @@ const updateUserAvatar = asyncHandler(async(req, res) => {
         throw new ApiError(400, "Avatar file is missing")
     }
 
-    //TODO: delete old image - assignment
+    //TODO: delete old image 
 
     const avatar = await uploadOnCloudinary(avatarLocalPath)
 
@@ -326,7 +326,7 @@ const updateUserCoverImage = asyncHandler(async(req, res) => {
         throw new ApiError(400, "Cover image file is missing")
     }
 
-    //TODO: delete old image - assignment
+    //TODO: delete old image - 
 
 
     const coverImage = await uploadOnCloudinary(coverImageLocalPath)
@@ -354,77 +354,77 @@ const updateUserCoverImage = asyncHandler(async(req, res) => {
 })
 
 
-// const getUserChannelProfile = asyncHandler(async(req, res) => {
-//     const {username} = req.params
+const getUserChannelProfile = asyncHandler(async(req, res) => {
+    const {username} = req.params
 
-//     if (!username?.trim()) {
-//         throw new ApiError(400, "username is missing")
-//     }
+    if (!username?.trim()) {
+        throw new ApiError(400, "username is missing")
+    }
 
-//     const channel = await User.aggregate([
-//         {
-//             $match: {
-//                 username: username?.toLowerCase()
-//             }
-//         },
-//         {
-//             $lookup: {
-//                 from: "subscriptions",
-//                 localField: "_id",
-//                 foreignField: "channel",
-//                 as: "subscribers"
-//             }
-//         },
-//         {
-//             $lookup: {
-//                 from: "subscriptions",
-//                 localField: "_id",
-//                 foreignField: "subscriber",
-//                 as: "subscribedTo"
-//             }
-//         },
-//         {
-//             $addFields: {
-//                 subscribersCount: {
-//                     $size: "$subscribers"
-//                 },
-//                 channelsSubscribedToCount: {
-//                     $size: "$subscribedTo"
-//                 },
-//                 isSubscribed: {
-//                     $cond: {
-//                         if: {$in: [req.user?._id, "$subscribers.subscriber"]},
-//                         then: true,
-//                         else: false
-//                     }
-//                 }
-//             }
-//         },
-//         {
-//             $project: {
-//                 fullName: 1,
-//                 username: 1,
-//                 subscribersCount: 1,
-//                 channelsSubscribedToCount: 1,
-//                 isSubscribed: 1,
-//                 avatar: 1,
-//                 coverImage: 1,
-//                 email: 1
+    const channel = await User.aggregate([
+        {
+            $match: {
+                username: username?.toLowerCase()
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+            }
+        },
+        {
+            $addFields: {
+                subscribersCount: {
+                    $size: "$subscribers"
+                },
+                channelsSubscribedToCount: {
+                    $size: "$subscribedTo"
+                },
+                isSubscribed: {
+                    $cond: {
+                        if: {$in: [req.user?._id, "$subscribers.subscriber"]},
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                fullName: 1,
+                username: 1,
+                subscribersCount: 1,
+                channelsSubscribedToCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1
 
-//             }
-//         }
-//     ])
+            }
+        }
+    ])
 
-//     if (!channel?.length) {
-//         throw new ApiError(404, "channel does not exists")
-//     }
+    if (!channel?.length) {
+        throw new ApiError(404, "channel does not exists")
+    }
 
-//     return res
-//     .status(200)
-//     .json(
-//         new ApiResponse(200, channel[0], "User channel fetched successfully")
-//     )
-// })
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, channel[0], "User channel fetched successfully")
+    )
+})
 
 // const getWatchHistory = asyncHandler(async(req, res) => {
 //     const user = await User.aggregate([
@@ -491,6 +491,6 @@ export {
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage,
-    // getUserChannelProfile,
+    getUserChannelProfile,
     // getWatchHistory
 }
