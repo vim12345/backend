0c33c91db066798ba86afa2fdf87d6f5ffdb9af0
vim12345 <vk2699945@gmail.com>
2024-01-20 01:55:30 +0530

    new

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, registerUser, logoutUser, refreshAccessToken} from "../controllers/user.controller.js";
+import { loginUser, registerUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 // import { verify } from "jsonwebtoken";
 import verify from 'jsonwebtoken';
@@ -28,5 +28,13 @@ router.route("/register").post(
  //secure routes
  router.route("/logout").post(verifyJWT, logoutUser)
  router.route("/refresh-token").post(refreshAccessToken)
+
+ router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+ router.route("/current-user").get(verifyJWT, getCurrentUser)
+ router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+ router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+ router.route("/cover-image").patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage)
+ router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+ router.route("/history").get(verifyJWT, getWatchHistory)
 
 export default router
